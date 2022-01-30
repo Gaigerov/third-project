dae90f4105d8c517011ae82c8b882a6c6ee3b0b1
Gaigerov <gaiger@Mac-Pro-Gaigerov.local>
2022-01-30 13:31:26 +0300

    third iteration

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const IDS = {
     NAME_INPUT: 'nameInput',
     JOB_INPUT: 'jobInput',
     NAME_PLACE_INPUT: 'namePlaceInput',
-    LINK_PLACE_INPUT: 'linkPlaceInput',
-  };
+    LINK_PLACE_INPUT: 'linkPlaceInput'
+};
 
 const CLASSES = {
     PROFILE_INFO_TITLE: 'profile-info__title',
@@ -26,12 +26,12 @@ const CLASSES = {
     IMAGE_POPUP_CONTAINER: 'image-popup__container',
     IMAGE_POPUP_IMAGE: 'image-popup__image',
     IMAGE_POPUP_TITLE: 'image-popup__image',
-    IMAGE_POPUP_CLOSE_ICON: 'image-popup__close-icon',
-  };
+    IMAGE_POPUP_CLOSE_ICON: 'image-popup__close-icon'
+};
 
 const EVENTS = {
     CLICK: 'click',
-    SUBMIT: 'submit',
+    SUBMIT: 'submit'
 };
 
 const nameInput = document.querySelector(`#${IDS.NAME_INPUT}`);
@@ -66,56 +66,56 @@ const elementLikeStatus = document.querySelector(`.${CLASSES.ELEMENT_LIKE_STATUS
 const elementTrashButton = document.querySelector(`.${CLASSES.ELEMENT_TRASH_BUTTON}`);
 
 const initialCards = [
-{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-},
-{
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-},
-{
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-},
-{
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-},
-{
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-},
-{
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-},
-]; 
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 const renderCard = (title, link) => {
-let elements = document.querySelector('.elements');
+    let elements = document.querySelector('.elements');
 
-let elementContainer = document.createElement('div');
-    elementContainer.className = "element";
-    
-let elementImage = document.createElement('img');
-    elementImage.className = "element__image";
+    let elementContainer = document.createElement('div');
+    elementContainer.className = 'element';
+
+    let elementImage = document.createElement('img');
+    elementImage.className = 'element__image';
     elementImage.src = link;
 
-let elementTrashButton = document.createElement('img');
-    elementTrashButton.className = "element__trash-button";
-    elementTrashButton.src = "./images/Trash.svg";
+    let elementTrashButton = document.createElement('img');
+    elementTrashButton.className = 'element__trash-button';
+    elementTrashButton.src = './images/Trash.svg';
 
-let elementSignature = document.createElement('div');
-    elementSignature.className = "element__signature";
+    let elementSignature = document.createElement('div');
+    elementSignature.className = 'element__signature';
 
-let elementText = document.createElement('p');
-    elementText.className = "element__text";
+    let elementText = document.createElement('p');
+    elementText.className = 'element__text';
     elementText.innerHTML = title;
-    
-let elementLikeStatus = document.createElement('img');
-    elementLikeStatus.className = "element__like-status";
-    elementLikeStatus.src = "./images/VectorHeart.svg";
+
+    let elementLikeStatus = document.createElement('img');
+    elementLikeStatus.className = 'element__like-status';
+    elementLikeStatus.src = './images/VectorHeart.svg';
 
     elements.append(elementContainer);
     elementContainer.append(elementImage);
@@ -124,82 +124,75 @@ let elementLikeStatus = document.createElement('img');
     elementSignature.append(elementText);
     elementSignature.append(elementLikeStatus);
 
-// смена статуса карточки ----------------------------
-let isLikeStatus = false;
+    // смена статуса карточки ----------------------------
+    let isLikeStatus = false;
 
-elementLikeStatus.addEventListener(EVENTS.CLICK, (evt) => {
-evt.preventDefault();
+    elementLikeStatus.addEventListener(EVENTS.CLICK, evt => {
+        evt.preventDefault();
 
-if (isLikeStatus || evt.target.classList.contains(elementLikeStatus)) {
-elementLikeStatus.src = './images/VectorHeart.svg';
-isLikeStatus = false;
-}
-else {
-elementLikeStatus.src = './images/Union.svg';
-isLikeStatus = true;
-}
-});
+        if (isLikeStatus || evt.target.classList.contains(elementLikeStatus)) {
+            elementLikeStatus.src = './images/VectorHeart.svg';
+            isLikeStatus = false;
+        } else {
+            elementLikeStatus.src = './images/Union.svg';
+            isLikeStatus = true;
+        }
+    });
 
-// открытие карточки ----------------------------
-elementImage.addEventListener(EVENTS.CLICK, (evt) => {
-    evt.preventDefault();
-    let imagePopup = document.querySelector('.image-popup');
-    
-    let imagePopupImage = document.createElement('img');
-    imagePopupImage.className = "image-popup__image";
-    imagePopupImage.src = elementImage.src;
-    let imagePopupTitle = document.createElement('p');
-    imagePopupTitle.className = "image-popup__title";
-    imagePopupTitle.innerHTML = elementText.textContent;
-    
-    imagePopup.append(imagePopupContainer);
-    imagePopupContainer.append(imagePopupImage);
-    imagePopupContainer.append(imagePopupTitle);
-    
-    imagePopup.classList.add('image-popup_opened');
-    })
-}
+    // открытие карточки ----------------------------
+    elementImage.addEventListener(EVENTS.CLICK, evt => {
+        evt.preventDefault();
+        let imagePopup = document.querySelector('.image-popup');
 
-const initCards = (cards) => {
+        let imagePopupImage = document.createElement('img');
+        imagePopupImage.className = 'image-popup__image';
+        imagePopupImage.src = elementImage.src;
+        let imagePopupTitle = document.createElement('p');
+        imagePopupTitle.className = 'image-popup__title';
+        imagePopupTitle.innerHTML = elementText.textContent;
+
+        imagePopup.append(imagePopupContainer);
+        imagePopupContainer.append(imagePopupImage);
+        imagePopupContainer.append(imagePopupTitle);
+
+        imagePopup.classList.add('image-popup_opened');
+    });
+};
+
+const initCards = cards => {
     elements.innerHTML = '';
     for (const card of cards) {
-    renderCard(card.name, card.link);      
-}
+        renderCard(card.name, card.link);
+    }
 };
-initCards(initialCards)
+initCards(initialCards);
 
 // закрытие карточки ----------------------------
-imagePopupCloseIcon.addEventListener(EVENTS.CLICK, (evt) => {
+imagePopupCloseIcon.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     imagePopupContainer.innerHTML = '';
     imagePopup.classList.remove('image-popup_opened');
-})
+});
 
 // добавление карточки ----------------------------
-addFormButton.addEventListener(EVENTS.CLICK, (evt) => {
+addFormButton.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
-    let card = {
-    name: namePlaceInput.value,
-    link: linkPlaceInput.value,
-}
-    initialCards.push(card);
-    renderCard(card.name, card.link);
-
+    renderCard(namePlaceInput.value, linkPlaceInput.value);
     addForm.classList.remove('addForm_opened');
 });
 
 // удаление карточки ----------------------------
-elements.addEventListener(EVENTS.CLICK, (evt) => {
+elements.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     const btn = evt.target.closest('.element__trash-button');
-        if (!btn) {
-    return;
-    }  
-        btn.parentElement.remove();
-    })
-    
+    if (!btn) {
+        return;
+    }
+    btn.parentElement.remove();
+});
+
 // открытие формы редактирования ----------------------------
-profileInfoEditButton.addEventListener(EVENTS.CLICK, (evt) => {
+profileInfoEditButton.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     nameInput.value = profileInfoTitle.innerHTML;
     jobInput.value = profileInfoSubtitle.innerHTML;
@@ -207,7 +200,7 @@ profileInfoEditButton.addEventListener(EVENTS.CLICK, (evt) => {
 });
 
 // сохранение изменений формы редактирования ----------------------------
-editFormButton.addEventListener(EVENTS.CLICK, (evt) => { 
+editFormButton.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     profileInfoTitle.textContent = nameInput.value;
     profileInfoSubtitle.textContent = jobInput.value;
@@ -215,21 +208,21 @@ editFormButton.addEventListener(EVENTS.CLICK, (evt) => {
 });
 
 // закрытие формы редактирования ----------------------------
-editFormCloseIcon.addEventListener(EVENTS.CLICK, (evt) => {
+editFormCloseIcon.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     editForm.classList.remove('editForm_opened');
 });
 
 // открытие формы добавления карточки ----------------------------
-profileAddButton.addEventListener(EVENTS.CLICK, (evt) => {
+profileAddButton.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     namePlaceInput.value = '';
     linkPlaceInput.value = '';
     addForm.classList.add('addForm_opened');
-})
+});
 
 // закрытие формы добавления карточки ----------------------------
-addFormCloseIcon.addEventListener(EVENTS.CLICK, (evt) => {
+addFormCloseIcon.addEventListener(EVENTS.CLICK, evt => {
     evt.preventDefault();
     addForm.classList.remove('addForm_opened');
 });
